37e974e3f99ab0c8d5cb88fbbf1844d86fd7994c
Zee <zee@debian.ifrn.local>
2025-02-13 14:12:32 -0300

    AulaNestMarcilio

---
+++ index.js
@@ -0,0 +1,35 @@
+//Para mais informações sobre o pg
+//https://node-postgres.com/apis/pool
+
+import pg from 'pg'
+//const pg = require('pg');
+
+const { Pool, Client } = pg
+
+const pool = new Pool({
+	host: 'localhost',
+	user: 'postgres',
+	database: 'aulaNestMarcilio',
+	password: 'ifrn.cn',
+	port: 5432,
+	max: 20,
+	idleTimeoutMillis: 30000,
+	connectionTimeoutMillis: 2000,
+})
+
+const client = await pool.connect()
+try {
+	await client.query('BEGIN')
+	const queryText = 'INSERT INTO Aluno (nome) VALUES($1) RETURNING idaluno'
+	const res = await client.query(queryText, ['Zee'])
+	const queryText2 = 'INSERT INTO Aluno (nome) VALUES($1) RETURNING idaluno'
+        const res2 = await client.query(queryText, ['Xuxa'])
+	const queryText3 = 'DELETE FROM Aluno WHERE idaluno = 2'
+        const res3 = await client.query(queryText3)
+	
+	await client.query('COMMIT')
+} catch (e) {
+	await client.query('ROLLBACK')
+} finally {
+	client.release()
+}
